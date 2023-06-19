2e56ce39412a0a58e2856adb02acae7bd3866ca3
Amay <brajbliss@gmail.com>
2023-06-19 21:41:50 +0530

    authenticate routes and navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
 import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+	RouterProvider,
+	createBrowserRouter,
+	Navigate,
+} from 'react-router-dom';
 import Register from './pages/auth/register/Register';
 import Login from './pages/auth/login/Login';
 import Home from './components/home/Home';
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Home />,
-	},
-	{
-		path: '/register',
-		element: <Register />,
-	},
-	{
-		path: '/login',
-		element: <Login />,
-	},
-]);
+import { useContext } from 'react';
+import { AuthContext } from './context/authContext';
 
 function App() {
+	const { currentUser } = useContext(AuthContext);
+
+	const router = createBrowserRouter([
+		{
+			path: '/',
+
+			element: currentUser ? <Home /> : <Navigate to='/login' />,
+			// element: <Home />,
+		},
+		{
+			path: '/register',
+			element: currentUser ? <Navigate to='/' /> : <Register />,
+		},
+		{
+			path: '/login',
+			element: currentUser ? <Navigate to='/' /> : <Login />,
+		},
+	]);
+
 	return (
 		<div className='app'>
 			<div className='container'>
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,24 @@
 import Notes from '../../pages/Notes/Notes';
 import Navbar from '../navbar/Navbar';
 import './home.scss';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
+import Login from '../../pages/auth/login/Login';
 
 const Home = () => {
+	const { currentUser } = useContext(AuthContext);
+
 	return (
+		// <div className='home'>
+		// 	{currentUser ? (
+		// 		<>
+		// 			<Navbar />
+		// 			<Notes />
+		// 		</>
+		// 	) : (
+		// 		<Login />
+		// 	)}
+		// </div>
 		<div className='home'>
 			<Navbar />
 			<Notes />

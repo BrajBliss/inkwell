e59e94980e6a9352011af8b4b19316e8a91b5273
Amay <brajbliss@gmail.com>
2023-06-20 19:59:24 +0530

    remove cors lib

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import authRoutes from './routes/auth.js';
 import noteRoutes from './routes/note.js';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 

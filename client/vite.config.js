86ae66a1692b00280dcd0ce6a60d43c90202a96a
Amay <brajbliss@gmail.com>
2023-06-20 16:32:17 +0530

    vercel express

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: 5173,
-		proxy: {
-			'/api': {
-				target: 'https://inkwell-3bpy.vercel.app',
-				changeOrigin: true,
-			},
-		},
-	},
+	// server: {
+	// 	port: 5173,
+	// 	proxy: {
+	// 		'/api': {
+	// 			target: 'https://inkwell-3bpy.vercel.app',
+	// 			changeOrigin: true,
+	// 		},
+	// 	},
+	// },
 });

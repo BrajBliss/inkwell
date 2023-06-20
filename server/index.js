849098d0f04959b94c8e592d8b4c7d44ac96a046
Amay <brajbliss@gmail.com>
2023-06-20 20:55:38 +0530

    port env

--- server/index.js
+++ server/index.js
@@ -16,4 +16,6 @@ mongoose
 app.use('/api/auth', authRoutes);
 app.use('/api/note', noteRoutes);
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Server is running on port 3000'));

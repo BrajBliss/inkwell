4263c185b32f551f418b201c31396c1bf424d71c
Amay <brajbliss@gmail.com>
2023-06-21 12:30:47 +0530

    another try at cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import authRoutes from './routes/auth.js';
 import noteRoutes from './routes/note.js';
 
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,26 +19,3 @@ app.use('/api/note', noteRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server is running on port 3000'));
-
-module.exports = (req, res) => {
-	//set header first to allow request or origin domain (value can be different)
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, PATCH, OPTIONS, DELETE'
-	);
-
-	//---- other code
-
-	//Preflight CORS handler
-	if (req.method === 'OPTIONS') {
-		return res.status(200).json({
-			body: 'OK',
-		});
-	}
-};

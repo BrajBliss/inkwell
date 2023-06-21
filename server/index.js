3baaf47c726c27d84fa7bbbf9501ea970b5b579f
Amay <brajbliss@gmail.com>
2023-06-21 12:13:29 +0530

    server cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import 'dotenv/config';
 import authRoutes from './routes/auth.js';
 import noteRoutes from './routes/note.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 

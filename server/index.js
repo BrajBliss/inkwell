71dbed4a3d9293e162181f3165b6b13bd2280246
Amay <brajbliss@gmail.com>
2023-06-21 12:26:41 +0530

    trying cors again

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import 'dotenv/config';
 import authRoutes from './routes/auth.js';
 import noteRoutes from './routes/note.js';
 
 const app = express();
-app.use(
-	cors({
-		origin: '*',
-	})
-);
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,3 +20,26 @@ app.use('/api/note', noteRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server is running on port 3000'));
+
+module.exports = (req, res) => {
+	//set header first to allow request or origin domain (value can be different)
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+	);
+
+	//---- other code
+
+	//Preflight CORS handler
+	if (req.method === 'OPTIONS') {
+		return res.status(200).json({
+			body: 'OK',
+		});
+	}
+};

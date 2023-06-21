634bc37896626b151e48e578e9b288fe81884e93
Amay <brajbliss@gmail.com>
2023-06-21 13:04:23 +0530

    edit vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// server: {
-	// 	port: 5173, // dev server on 5173
-	// 	proxy: {
-	// 		'/api/': {
-	// 			target: 'https://inkwell-server.vercel.app/',
-	// 			changeOrigin: true,
-	// 		},
-	// 	},
-	// },
+	server: {
+		proxy: {
+			'/api/': {
+				target: 'https://inkwell-server.vercel.app/',
+				changeOrigin: true,
+			},
+		},
+	},
 });

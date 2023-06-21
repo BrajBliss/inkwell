7d798b96d216075e2df7ab01cb23e208af0c6327
Amay <brajbliss@gmail.com>
2023-06-21 17:05:30 +0530

    cookie

--- server/index.js
+++ server/index.js
@@ -6,16 +6,25 @@ import authRoutes from './routes/auth.js';
 import noteRoutes from './routes/note.js';
 
 const app = express();
+// app.use(function (req, res, next) {
+// 	res.header('Access-Control-Allow-Credentials', true);
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Methods', '*');
+// 	res.header(
+// 		'Access-Control-Allow-Headers',
+// 		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+// 	);
+// 	next();
+// });
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 	res.header(
 		'Access-Control-Allow-Headers',
-		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+		'Origin, X-Requested-With, Content-Type, Accept'
 	);
 	next();
 });
+
 app.use(express.json());
 app.use(cookieParser());
 

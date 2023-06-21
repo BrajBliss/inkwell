79a6b72d51c2667c976ecf76f53cefd7859128fd
Amay <brajbliss@gmail.com>
2023-06-21 18:12:50 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -6,16 +6,20 @@ import authRoutes from './routes/auth.js';
 import noteRoutes from './routes/note.js';
 
 const app = express();
-// app.use(function (req, res, next) {
-// 	res.header('Access-Control-Allow-Credentials', true);
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Methods', '*');
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
-// 	);
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'ORIGIN');
+	res.header(
+		'Access-Control-Allow-Methods',
+		'PUT, GET, HEAD, POST, PATCH, DELETE, CONNECT, OPTIONS'
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, *'
+	);
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Expose-Headers', '*');
+	next();
+});
 
 app.use(express.json());
 app.use(cookieParser());

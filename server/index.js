9ebf3dd2fe7c98460572e32f798950f6df3a5172
Amay <brajbliss@gmail.com>
2023-06-21 12:45:06 +0530

    idk at this point

--- server/index.js
+++ server/index.js
@@ -18,4 +18,4 @@ app.use('/api/note', noteRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Server is running on port 3000'));
+app.listen(PORT, () => console.log('Server is running on port ' + PORT));

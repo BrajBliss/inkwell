f1d0371412ebc0e1e8afb830465e623ada5777be
Amay <brajbliss@gmail.com>
2023-06-23 20:18:42 +0530

    validator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import { useContext } from 'react';
 import { AuthContext } from './context/authContext';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://inkwell-server.vercel.app/';
 axios.defaults.withCredentials = true;
 
 function App() {

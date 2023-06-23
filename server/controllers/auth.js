06c9ec78dc9ac00bc3b1dcaa1a4b89073ba78788
Amay <brajbliss@gmail.com>
2023-06-23 17:07:45 +0530

    cookie domain full

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ export const login = async (req, res) => {
 					httpOnly: true,
 					sameSite: 'none',
 					secure: true,
-					domain: '.netlify.app',
+					domain: 'https://ink-well.netlify.app/',
 				})
 					.status(200)
 					.json('logged in successfully');
